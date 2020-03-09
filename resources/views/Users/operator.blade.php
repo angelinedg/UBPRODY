630ffedd8078b75faddc5f2cfa663738b4f4b15b
angelinedg <60335997+angelinedg@users.noreply.github.com>
2020-03-09 17:29:12 +0800

    FINISHED CRUD FOR OPERATOR

---
+++ resources/views/Users/operator.blade.php
@@ -0,0 +1,15 @@
+@include('layouts.headerlog')
+@section('content')
+<body>
+	<div class="container" style="width:80%; margin-right: 0px; margin-left: 0px">
+    	<div class="row justify-content-center">
+    		<div class="col-md-12">
+			@include('operatorui')
+				    </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+
+	 
